debf475c09b00d7d6214bf4a4d7ed6e89b88f0d6
unknown <pengtaojiang@163.com>
2022-04-13 15:29:38 +0800

    .

--- gen_gt_coco.sh
+++ gen_gt_coco.sh
@@ -6,6 +6,6 @@ CUDA_VISIBLE_DEVICES=0 python3 gen_gt.py \
    --dataset=mscoco \
    --datalist=./data/coco14/train_cls.txt \
    --gt_dir=./data/coco14/SegmentationClass/ \
-   --save_path=./runs/${EXP}/pseudo_seg_labels \
+   --save_path=./data/coco14/pseudo_seg_labels/ \
    --pred_dir=./runs/${EXP}/${TYPE}/attention/ \
    --num_workers=16
--- gen_gt_voc.sh
+++ gen_gt_voc.sh
@@ -6,6 +6,6 @@ CUDA_VISIBLE_DEVICES=0 python3 gen_gt.py \
    --dataset=mscoco \
    --datalist=data/voc12/train_aug.txt \
    --gt_dir=./data/voc12/JPEGImages/ \
-   --save_path=./runs/${EXP}/pseudo_seg_labels/ \
+   --save_path=./data/voc12/pseudo_seg_labels/ \
    --pred_dir=./runs/${EXP}/${TYPE}/attention/ \
    --num_workers=16
